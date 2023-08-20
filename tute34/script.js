4c6296b0045febac2a9d857420fb71da08f7c5c6
Dilshad <khandilshad1225@gmail.com>
2023-08-20 14:13:20 +0530

    Added tutorial 34 and 35

---
+++ tute34/script.js
@@ -0,0 +1,12 @@
+const changebgcolor=()=>{
+    document.body.firstElementChild.style.background="red";
+}
+changebgcolor();
+// to run this run changebgcolor() in browser console or call function here
+
+
+let b= document.body;
+console.log("first child of b is : ", b.firstChild);  // return any type of first child
+console.log("first element child of b is  : ", b.firstElementChild);
+
+
