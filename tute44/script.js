8709548d8ce1335fcb332baa9f31b607ee64a035
Dilshad <khandilshad1225@gmail.com>
2023-08-24 16:20:00 +0530

    added tutorial 44

---
+++ tute44/script.js
@@ -0,0 +1,6 @@
+// first.insertAdjacentHTML('beforebegin','<div class="test">beforeBegin</div>'); // out of the tag 
+// first.insertAdjacentHTML('beforeend','<div class="test">beforeEnd</div>');  // in the tag
+// first.insertAdjacentHTML('afterbegin','<div class="test">afterBegin</div>');  // in the tag
+// first.insertAdjacentHTML('afterend','<div class="test">afterEnd</div>');   // after tag
+
+first.remove();  // remove the Node
