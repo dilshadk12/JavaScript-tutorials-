2fdb01babaac0210ad644a55550fdac25e45b417
Dilshad <khandilshad1225@gmail.com>
2023-08-25 20:36:19 +0530

    added tutorial 45

---
+++ tute45/script.js
@@ -0,0 +1,10 @@
+first.className="text-dark red";  // on execution it will turn background red and color black
+
+first.classList.remove("red");  // remove individual class
+first.classList.add("red");  // add individual class
+
+first.classList.toggle("red"); // if red is present then it will remove it 
+                              // if red not present then it will add it.
+
+first.classList.contains("red");  // check whether class present or not
+          
