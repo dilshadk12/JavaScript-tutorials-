6539f0206c4cdcec0b55b8db77c3606986211ef0
Dilshad <khandilshad1225@gmail.com>
2023-09-16 14:09:31 +0530

    added tutorial 60

---
+++ tute60/script.js
@@ -0,0 +1,25 @@
+setTimeout(() => {
+	console.log("Hacking wifi...Please wait");
+}, 1000)
+
+// console.log(rahul);   // error generated and code won't run beyond here 
+// To solve this
+
+try {
+	console.log(rahul);
+}
+catch (error) {
+	console.log(error);
+}
+
+setTimeout(() => {
+	console.log("Fetching UserName and password...Please wait");
+}, 2000)
+
+setTimeout(() => {
+	console.log("Hacking Rahul's Facebook I'd...Please wait");
+}, 3000)
+
+setTimeout(() => {
+	console.log("UserName and Password of Rahul: (+91923456789)...Please wait");
+}, 4000) 
